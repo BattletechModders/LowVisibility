1f7cdf355033e191362a4cc0ac624256d888f44b
Gerrit Kitts <gkitts@gmail.com>
2019-01-26 18:40:58 -0500

    Added MathHelpers

---
+++ LowVisibility/LVUnitTests/HexesTests.cs
@@ -0,0 +1,88 @@
+﻿using LowVisibility.Helper;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace LVUnitTests {
+
+    [TestFixture]
+    public class Test {
+
+        [Test]
+        public void DivVsIter() {
+            List<float> ranges = RangesInMeters();
+            List<int> divHexes = new List<int>();
+            // Division
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            foreach (float range in ranges) {
+                divHexes.Add(RangeToHexByDiv(range));
+            }
+
+            stopWatch.Stop();
+            long divTime = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine($"Division time was:{divTime}ms");
+
+            // Iteration
+            List<int> iterHexes = new List<int>();
+            stopWatch.Reset();
+            stopWatch.Start();
+            foreach (float range in ranges) {
+                iterHexes.Add(MathHelper.CountHexes(range));
+            }
+
+            stopWatch.Stop();
+            long iterTime = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine($"Iteration time was:{iterTime}ms");
+
+            float[] rangeArr = ranges.ToArray();
+            int[] divArray = divHexes.ToArray();
+            int[] iterArray = iterHexes.ToArray();
+            Console.WriteLine($"Testing array lengths");
+            Assert.AreEqual(divArray.Length, iterArray.Length);
+
+            Console.WriteLine($"Testing array values");
+            for (int i = 0; i < divArray.Length; i++) {
+                Console.WriteLine($"range:{rangeArr[i]} div:{divArray[i]} iter:{iterArray[i]}");
+                Assert.AreEqual(divArray[i], iterArray[i]);
+            }
+
+            //Console.WriteLine($"Testing execution time");
+            //Assert.AreEqual(divTime, iterTime);
+        }
+
+        private int RangeToHexByDiv(float range) {
+            int hexes = 0;
+            if (range % 30.0 == 0) {
+                hexes = (int)Math.Floor(range / 30.0);
+            } else {
+                hexes = (int)Math.Floor(range / 30);
+                hexes = hexes + 1;
+            }
+            return hexes;
+        }
+
+        private int RangeToHexByIter(float range) {
+            int hexes = 0;
+            int count = 0;
+            while (count < range) {
+                count += 30;
+                hexes++;
+            }
+            return hexes;
+        }
+
+
+        private List<float> RangesInMeters() {
+            List<float> ranges = new List<float>();
+            Random rand = new Random();
+            for (int i = 0; i < 1000; i++) {
+                float range = rand.Next(1, 500);
+                ranges.Add(range);
+            }
+
+            return ranges;
+        }
+    }
+}
