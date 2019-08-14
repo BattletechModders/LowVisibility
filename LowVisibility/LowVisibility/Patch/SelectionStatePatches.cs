ea15fbfa330277073e0e15225317e05313b19fde
Gerrit Kitts <gkitts@gmail.com>
2019-08-13 23:29:13 -0400

    - Added restrictions to multi-targeting; cannot select a unit that has stealth (players only?) - Added restrictions to called shot; cannot select a unit that is a blip or when sensor info < SurfaceScan (players only?) - WIP: Apply restrictions to AI

--- LowVisibility/LowVisibility/Patch/SelectionStatePatches.cs
+++ LowVisibility/LowVisibility/Patch/SelectionStatePatches.cs
@@ -7,49 +7,50 @@ using us.frostraptor.modUtils;
 
 namespace LowVisibility.Patch {
 
-    [HarmonyPatch(typeof(SelectionStateFireMulti), "ProcessClickedCombatant")]
-    public static class SelectionStateFireMulti_ProcessClickedCombatant {
-        public static void Postfix(SelectionStateFireMulti __instance, ref bool __value, ICombatant actor) {
-            Mod.Log.Trace("SSFM:PCC entered");
-
-            // Prevents units with stealth or mimetic from being multi-targeted
-            if (__instance != null && actor != null && actor is AbstractActor aActor) {
-                EWState targetState = new EWState(aActor);
-                if (targetState.HasStealth() || targetState.HasMimetic()) {
-                    Mod.Log.Info($"Target {CombatantUtils.Label(actor)} has stealth, cannot be multi-targeted!");
-                    __value = false;
-                    __instance.BackOut();
-                }
-            }
-        }
-    }
-    
+    [HarmonyPatch(typeof(SelectionStateFire), "ProcessClickedCombatant")]
     public static class SelectionStateFire_ProcessClickedCombatant {
-        public static void Postfix(SelectionStateFireMulti __instance, ref bool __value, ICombatant actor) {
-            Mod.Log.Trace("SSF:PCC entered");
 
-            if (__instance != null && actor != null && actor is AbstractActor aActor && 
-                __instance.SelectionType == SelectionType.FireMorale) {
+        public static bool Prefix(SelectionStateFire __instance, ref bool __result, ICombatant combatant) {
+            Mod.Log.Info("SSF:PCC:PRE entered");
 
-                EWState targetState = new EWState(aActor);
+            if (__instance != null && combatant != null && combatant is AbstractActor targetActor && __instance.SelectedActor != null) {
+
+                CombatGameState Combat = __instance.SelectedActor.Combat;
+                bool targetIsFriendly = Combat.HostilityMatrix.IsFriendly(combatant.team.GUID, Combat.LocalPlayerTeamGuid);
+                if (targetIsFriendly) {
+                    Mod.Log.Trace("Friendly target, skipping check");
+                    return true;
+                }
+
+                EWState targetState = new EWState(targetActor);
                 EWState attackerState = new EWState(__instance.SelectedActor);
 
-                // Prevents blips from being the targets of called shots
-                VisibilityLevel targetVisibility = __instance.SelectedActor.VisibilityToTargetUnit(actor);
-                if (targetVisibility < VisibilityLevel.LOSFull) {
-                    Mod.Log.Info($"Target {CombatantUtils.Label(actor)} is a blip, cannot be targeted by called shot");
-                    __value = false;
-                    __instance.BackOut();
-                }
+                if (__instance.SelectionType == SelectionType.FireMorale) {
+                    // Prevents blips from being the targets of called shots
+                    VisibilityLevel targetVisibility = __instance.SelectedActor.VisibilityToTargetUnit(targetActor);
+                    if (targetVisibility < VisibilityLevel.LOSFull) {
+                        Mod.Log.Info($"Target {CombatantUtils.Label(combatant)} is a blip, cannot be targeted by called shot");
+                        __result = false;
+                        return false;
+                    }
 
-                SensorScanType sensorScan = SensorLockHelper.CalculateSharedLock(actor, __instance.SelectedActor);
-                if (sensorScan < SensorScanType.SurfaceScan) {
-                    Mod.Log.Info($"Target {CombatantUtils.Label(actor)} sensor info {sensorScan} is less than SurfaceScan, cannot be targeted by called shot");
-                    __value = false;
-                    __instance.BackOut();
+                    SensorScanType sensorScan = SensorLockHelper.CalculateSharedLock(targetActor, __instance.SelectedActor);
+                    if (sensorScan < SensorScanType.SurfaceScan) {
+                        Mod.Log.Info($"Target {CombatantUtils.Label(targetActor)} sensor info {sensorScan} is less than SurfaceScan, cannot be targeted by called shot");
+                        __result = false;
+                        return false;
+                    }
+                } else if (__instance.SelectionType == SelectionType.FireMulti) {
+                    if (targetState.HasStealth() || targetState.HasMimetic()) {
+                        Mod.Log.Info($"Target {CombatantUtils.Label(targetActor)} has stealth, cannot be multi-targeted!");
+                        __result = false;
+                        return false;
+                    }
                 }
             }
 
+            __result = false;
+            return true;
         }
     }
 
