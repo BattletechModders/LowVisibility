83b34a5b108fefe5994542a67346992287912306
Gerrit Kitts <gkitts@gmail.com>
2020-01-22 18:50:00 -0500

    More ECM carrier cleanup

--- LowVisibility/LowVisibility/Patch/PilotableActorRepresentationPatches.cs
+++ LowVisibility/LowVisibility/Patch/PilotableActorRepresentationPatches.cs
@@ -37,10 +37,6 @@ namespace LowVisibility.Patch {
                     VfxHelper.DisableMimeticEffect(parentActor);
                 }
 
-                if (parentState.IsECMCarrier()) {
-                    VfxHelper.EnableECMCarrierVfx(parentActor, parentState.ECMCarrierRange());
-                }
-
             } else if (newLevel >= VisibilityLevel.Blip0Minimum) {
                 Mod.Log.Debug($"Actor: {CombatantUtils.Label(parentActor)} has changed visibility to: {newLevel}");
 
