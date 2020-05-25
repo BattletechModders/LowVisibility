e0696c0b40eaab5583611a80464c2c9eaf8e0bb4
Gerrit Kitts <gkitts@gmail.com>
2020-05-25 11:26:23 -0400

    - Displaying the image now

--- LowVisibility/LowVisibility/Patch/HUD/CombatHUDMarkDisplay.cs
+++ LowVisibility/LowVisibility/Patch/HUD/CombatHUDMarkDisplay.cs
@@ -34,6 +34,13 @@ namespace LowVisibility.Patch.HUD
                 bool isEnemy = ModState.Combat.HostilityMatrix.IsLocalPlayerEnemy(__instance.DisplayedActor.team);
                 if (isEnemy)
                 {
+
+                    // Check that enemy is visible (blip or better)
+                    Vector3 newPos = __instance.gameObject.transform.position;
+
+                    VerticalLayoutGroup vlg = __instance.gameObject.transform.parent.gameObject.GetComponent<VerticalLayoutGroup>();
+                    vlg.spacing = 4f;
+
                     Mod.Log.Info($"UPDATING COMBATHUDMARKDISPLAY FOR ACTOR: {CombatantUtils.Label(__instance.DisplayedActor)}");
                     // Cache these
                     AbstractActor target = __instance.DisplayedActor;
@@ -43,18 +50,48 @@ namespace LowVisibility.Patch.HUD
                     if (icon == null) Mod.Log.Error("FAILED TO LOAD ICON!");
                     GameObject imageGO = new GameObject();
                     imageGO.name = "lv_icon_foo";
-                    imageGO.transform.parent = __instance.gameObject.transform;
-                    imageGO.transform.position = __instance.gameObject.transform.position;
-                    //imageGO.transform.localScale = new Vector3(1f, 1f, 1f);
+                    imageGO.transform.parent = __instance.gameObject.transform.parent;
+                    //newPos.x += 30f;
+                    //imageGO.transform.position = newPos;
+                    //imageGO.transform.localScale = new Vector3(0.6f, 0.6f, 1f);
+                    imageGO.transform.localScale = new Vector3(0.35f, 1f, 1f);
 
                     SVGImage image = imageGO.AddComponent<SVGImage>();
                     if (image == null) Mod.Log.Error("FAILED TO CREATE IMAGE!");
                     image.vectorGraphics = icon;
                     image.color = Color.green;
-                    image.gameObject.SetActive(true);
+                    image.enabled = true;
+                    imageGO.SetActive(true);
+                    Mod.Log.Info("Set objects active!");
+
+                    LayoutElement le = imageGO.AddComponent<LayoutElement>();
+                    le.preferredHeight = 16f;
+                    le.preferredWidth = 16f;
 
                     RectTransform rectTransform = imageGO.GetComponent<RectTransform>();
-                    rectTransform.sizeDelta = new Vector2(28f, 28f);                    
+                    rectTransform.sizeDelta = new Vector2(50f, 0f);
+                    Mod.Log.Info("Set SIZE DELTA");
+
+                    //SVGAsset icon2 = ModState.Combat.DataManager.GetObjectOfType<SVGAsset>(Mod.Config.Icons.TargetSensorsMark, BattleTechResourceType.SVGAsset);
+                    //if (icon2 == null) Mod.Log.Error("FAILED TO LOAD ICON 2!");
+                    //GameObject imageGO2 = new GameObject();
+                    //imageGO2.name = "lv_icon_foo2";
+                    //imageGO2.transform.parent = __instance.gameObject.transform.parent;
+                    //newPos.x += 30f;
+                    //imageGO.transform.position = newPos;
+                    ////imageGO.transform.localScale = new Vector3(1f, 1f, 1f);
+
+                    //SVGImage image2 = imageGO2.AddComponent<SVGImage>();
+                    //if (image2 == null) Mod.Log.Error("FAILED TO CREATE IMAGE 2!");
+                    //image2.vectorGraphics = icon;
+                    //image2.color = Color.green;
+                    //image.enabled = true;
+                    //imageGO.SetActive(true);
+                    //Mod.Log.Info("Set objects active 2!");
+
+                    //RectTransform rectTransform2 = imageGO.GetComponent<RectTransform>();
+                    //rectTransform2.sizeDelta = new Vector2(28f, 28f);
+                    //Mod.Log.Info("Set SIZE DELTA");
 
                     //bool hasSensorAttack = SensorLockHelper.CalculateSharedLock(target, attacker) > SensorScanType.NoInfo;
                     //if (hasSensorAttack)
@@ -110,7 +147,7 @@ namespace LowVisibility.Patch.HUD
                     //        __instance.MarkIcons.Add(image);
                     //    }
                     //}
-                    
+
 
                 }
             }
