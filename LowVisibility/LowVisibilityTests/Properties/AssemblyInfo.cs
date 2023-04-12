25804d760a542047e08bc255375916f0b61bec96
IceRaptor <gkitts@gmail.com>
2023-04-12 18:14:17 -0400

    - Update to MS 2019 project format

--- LowVisibility/LowVisibilityTests/Properties/AssemblyInfo.cs
+++ LowVisibility/LowVisibilityTests/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("LowVisibilityTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LowVisibilityTests")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("06ab0513-a99d-4bf1-a4bd-ccff7fb5169a")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
